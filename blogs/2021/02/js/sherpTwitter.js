c5f60fb37443ea3d56ad7c03126ff147c3f18b5b
nmccarty3 <NMCCARTY@CALTECH.EDU>
2021-02-24 00:03:46 -0500

    changed tooltip y-axis position

--- blogs/2021/02/js/sherpTwitter.js
+++ blogs/2021/02/js/sherpTwitter.js
@@ -189,7 +189,7 @@ async function drawScatter() {
         + dimensions.margin.left 
       const tooltipY = yScale(yAccessor(d))
         + dimensions.margin.top
-        + 180;
+        + 120;
   
       tooltip.style("transform", `translate(`
         + `calc( -50% + ${tooltipX}px),`
